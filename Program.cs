bdf9a6faa6b0d305f2c40892b2ee28bc3cc5a46b
RasmusFPS <Rasmus.andersen05@gmail.com>
2025-09-17 10:03:53 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography;
+
+namespace MonsterGen;
+
+class Program
+{
+    public class Monster()
+    {
+        public int atk;
+        public int hp;
+        public string name;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the monsterGen");
+    }
+
+
+    public void MonsterGenerator()
+    {
+        Random rnd = new Random();
+        string[] monstersN = { "Bronzor", "Chramander", "gible", "Frog" };
+        int Names = rnd.Next(monstersN.Length);
+    }
+
+
+}
